47b1627ee5e4ef3b8468f4eb93bda5919a17bac0
qiugang <thisisqg@gmail.com>
2016-04-08 01:08:58 +0800

    init project

---
+++ README.md
@@ -0,0 +1,57 @@
+# EditTag
+ Just use it to edit your tags like EditText edit your text.
+### Demo
+<img src="/screenshots/s01.jpg" width="360" height="640" />
+### Step 1
+
+* Add EditTag View in your layout resource
+
+```xml
+
+ <me.originqiu.library.EditTag
+        android:id="@+id/edit_tag_view"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        app:tag_layout="@layout/view_sample_tag"
+        app:input_layout="@layout/view_sample_input_tag"/>
+```
+
+### Step 2
+
+* Custom your Tag ,Input EditText layout resource
+* Set  tag list : ```setTagList(List<String> yourTags)```
+* Get tag list: ```getTagList()```
+
+### Todo
+ - [ ] Delete single tag out of order
+ - [ ] Tag click event
+
+
+### Thanks
+* [FlowLayout](https://github.com/hongyangAndroid/FlowLayout/blob/master/flowlayout-lib%2Fsrc%2Fmain%2Fjava%2Fcom%2Fzhy%2Fview%2Fflowlayout%2FFlowLayout.java)
+
+###License
+    The MIT License (MIT)
+
+        Copyright (c) 2015 OriginQiu
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+
+
+
---
+++ library/src/main/java/me/originqiu/library/EditTag.java
@@ -0,0 +1,175 @@
+/**
+ * The MIT License (MIT)
+
+ Copyright (c) 2015 OriginQiu
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+package me.originqiu.library;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by OriginQiu on 4/7/16.
+ */
+public class EditTag extends FrameLayout {
+    
+    private FlowLayout mFlowLayout;
+    
+    private EditText mEditText;
+    
+    private int tagViewLayoutRes;
+    
+    private int inputTagLayoutRes;
+    
+    private List<String> mTagList = new ArrayList<>();
+    
+    public EditTag(Context context) {
+        this(context, null);
+    }
+    
+    public EditTag(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+    
+    public EditTag(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray mTypedArray = context.obtainStyledAttributes(attrs,
+                                                                R.styleable.EditTag);
+        tagViewLayoutRes = mTypedArray.getResourceId(R.styleable.EditTag_tag_layout,
+                                                     R.layout.view_default_tag);
+        inputTagLayoutRes = mTypedArray.getResourceId(R.styleable.EditTag_input_layout,
+                                                      R.layout.view_default_input_tag);
+        setupView();
+    }
+    
+    private void setupView() {
+        mFlowLayout = new FlowLayout(getContext());
+        LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT,
+                                                     LayoutParams.WRAP_CONTENT);
+        mFlowLayout.setLayoutParams(layoutParams);
+        addView(mFlowLayout);
+        addTagView();
+        setupListener();
+    }
+    
+    private void setupListener() {
+        mEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            
+            @Override
+            public boolean onEditorAction(TextView v,
+                                          int actionId,
+                                          KeyEvent event) {
+                boolean isHandle = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    String tagContent = mEditText.getText().toString();
+                    if (TextUtils.isEmpty(tagContent)) {
+                        // do nothing, or you can tip "can'nt add empty tag"
+                    }
+                    else {
+                        mFlowLayout.addView(createTag(mFlowLayout, tagContent),
+                                            mFlowLayout.getChildCount() - 1);
+                        isHandle = true;
+                        mTagList.add(tagContent);
+                        mEditText.getText().clear();
+                    }
+                }
+                return isHandle;
+            }
+        });
+        mEditText.setOnKeyListener(new View.OnKeyListener() {
+            
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                boolean isHandle = false;
+                if (keyCode == KeyEvent.KEYCODE_DEL && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    String tagContent = mEditText.getText().toString();
+                    if (TextUtils.isEmpty(tagContent)) {
+                        // if tags != null,delete last tag
+                        int tagCount = mFlowLayout.getChildCount();
+                        if (tagCount > 1) {
+                            mFlowLayout.removeViewAt(tagCount - 2);
+                            mTagList.remove(tagCount - 2);
+                            isHandle = true;
+                        }
+                    }
+                    else {
+                        mEditText.getText().delete(tagContent.length() - 1,
+                                                   tagContent.length());
+                    }
+                }
+                return isHandle;
+            }
+        });
+    }
+    
+    private void addTagView() {
+        mFlowLayout.removeAllViews();
+        int size = mTagList.size();
+        for (int i = 0; i < size + 1; i++) {
+            if (i == size) {
+                mFlowLayout.addView(createEditTag(mFlowLayout));
+            }
+            else {
+                mFlowLayout.addView(createTag(mFlowLayout, mTagList.get(i)));
+            }
+        }
+    }
+    
+    private TextView createTag(ViewGroup parent, String s) {
+        TextView tagTv = (TextView) LayoutInflater.from(getContext())
+                                                  .inflate(tagViewLayoutRes,
+                                                           parent,
+                                                           false);
+        tagTv.setText(s);
+        return tagTv;
+    }
+    
+    private EditText createEditTag(ViewGroup parent) {
+        mEditText = (EditText) LayoutInflater.from(getContext())
+                                             .inflate(inputTagLayoutRes,
+                                                      parent,
+                                                      false);
+        return mEditText;
+    }
+
+    public List<String> getTagList() {
+        return mTagList;
+    }
+
+    public void setTagList(List<String> mTagList) {
+        this.mTagList = mTagList;
+        addTagView();
+    }
+}
