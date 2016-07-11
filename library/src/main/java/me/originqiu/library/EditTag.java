ff744ecbcdb76ae07dc6185d61c3b3a6c254a1bc
qiugang <thisisqg@gmail.com>
2016-07-11 14:25:04 +0800

    save local

--- library/src/main/java/me/originqiu/library/EditTag.java
+++ library/src/main/java/me/originqiu/library/EditTag.java
@@ -1,33 +1,29 @@
 /**
- * The MIT License (MIT)
-
- Copyright (c) 2015 OriginQiu
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
-
+ * The MIT License (MIT) Copyright (c) 2015 OriginQiu Permission is hereby
+ * granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions: The above copyright notice and this
+ * permission notice shall be included in all copies or substantial portions of
+ * the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  */
 package me.originqiu.library;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,6 +32,7 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,6 +49,8 @@ public class EditTag extends FrameLayout {
     private int tagViewLayoutRes;
     
     private int inputTagLayoutRes;
+    
+    private boolean isEditableStatus = true;
     
     private List<String> mTagList = new ArrayList<>();
     
@@ -72,9 +71,9 @@ public class EditTag extends FrameLayout {
         inputTagLayoutRes = mTypedArray.getResourceId(R.styleable.EditTag_input_layout,
                                                       R.layout.view_default_input_tag);
         mTypedArray.recycle();
-
+        
         setupView();
-
+        
     }
     
     private void setupView() {
@@ -82,18 +81,24 @@ public class EditTag extends FrameLayout {
         LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT,
                                                      LayoutParams.WRAP_CONTENT);
         mFlowLayout.setLayoutParams(layoutParams);
-
         addView(mFlowLayout);
         addTagView();
         setupListener();
-
     }
-
-
-    public void setEditable(boolean editable){
-        if(!editable && mFlowLayout.getChildCount()>0){
-            mFlowLayout.removeViewAt(mFlowLayout.getChildCount()-1);
+    
+    public void setEditable(boolean editable) {
+        if (editable) {
+            if (!isEditableStatus) {
+                mFlowLayout.addView(createEditTag(mFlowLayout));
+                setupListener();
+            }
+        } else {
+            int childCount = mFlowLayout.getChildCount();
+            if (isEditableStatus && childCount > 0) {
+                mFlowLayout.removeViewAt(childCount - 1);
+            }
         }
+        this.isEditableStatus = editable;
     }
     
     private void setupListener() {
@@ -126,6 +131,7 @@ public class EditTag extends FrameLayout {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 boolean isHandle = false;
                 if (keyCode == KeyEvent.KEYCODE_DEL && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    Log.d("delete","deleteAction");
                     String tagContent = mEditText.getText().toString();
                     if (TextUtils.isEmpty(tagContent)) {
                         // if tags != null,delete last tag
@@ -155,14 +161,16 @@ public class EditTag extends FrameLayout {
             }
             else {
                 mFlowLayout.addView(createTag(mFlowLayout, mTagList.get(i)));
+                isEditableStatus = true;
             }
         }
     }
     
     private TextView createTag(ViewGroup parent, String s) {
-        TextView tagTv = (TextView) LayoutInflater.from(getContext()).inflate(tagViewLayoutRes,
-                        parent,
-                        false);
+        TextView tagTv = (TextView) LayoutInflater.from(getContext())
+                                                  .inflate(tagViewLayoutRes,
+                                                           parent,
+                                                           false);
         tagTv.setText(s);
         return tagTv;
     }
@@ -174,11 +182,11 @@ public class EditTag extends FrameLayout {
                                                       false);
         return mEditText;
     }
-
+    
     public List<String> getTagList() {
         return mTagList;
     }
-
+    
     public void setTagList(List<String> mTagList) {
         this.mTagList = mTagList;
         addTagView();
--- sample/src/main/java/me/originqiu/edittag/MainActivity.java
+++ sample/src/main/java/me/originqiu/edittag/MainActivity.java
@@ -2,12 +2,39 @@ package me.originqiu.edittag;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.widget.CompoundButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import me.originqiu.library.EditTag;
+
+public class MainActivity extends AppCompatActivity implements SwitchCompat.OnCheckedChangeListener {
+
+    private EditTag editTagView;
+
+    private SwitchCompat statusSwitchView;
+
+    private List<String> tagStrings = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTagView = (EditTag) findViewById(R.id.edit_tag_view);
+        statusSwitchView = (SwitchCompat) findViewById(R.id.status_switch);
+        statusSwitchView.setOnCheckedChangeListener(this);
+
+        for (int i = 0; i < 10; i++) {
+            tagStrings.add("test" + i);
+        }
+        editTagView.setTagList(tagStrings);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        editTagView.setEditable(isChecked);
     }
 }
